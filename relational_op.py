84cf762ee7849ae57f6b991d2cb64c497d226734
csamal7 <165898446+csamal7@users.noreply.github.com>
2024-04-12 10:55:02 +0530

    Create relational_operators

---
+++ relational_op.py
@@ -0,0 +1,16 @@
+a = 5
+b = 2
+
+less_than = a < b
+greater_than = a > b
+less_than_equal = a <= b
+greater_than_equal = a >= b
+equal = a == b
+not_equal = a != b
+
+print("a < b : ", less_than)
+print("a > b : ", greater_than)
+print("a <= b : ", less_than_equal)
+print("a >= b : ", greater_than_equal)
+print("a == b : ", equal)
+print("a != b : ", not_equal)
